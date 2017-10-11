87271cc75745cdb2802f93352e7a5881145199af
Pedro Faria <pfaf09@gmail.com>
2017-10-11 22:41:00 +0100

    Fixed display. Changed to show Queen.

--- Trabalho1/home.pl
+++ Trabalho1/home.pl
@@ -1,32 +1,68 @@
-start :- board(B1),
-        display_first_line("A","H"), nl,
+start :- middle(B1),
+        display_first_line("A","H",B1), nl,
         board_display(1,B1).
-
 
 
 % =========== BOARDS ===========
 board([[v, v, v, v, v, v, v, v],
-      [blank, v, v, v, v, v, v, blank],
-      [blank, blank, v, v, v, v, blank, blank],
-      [blank, blank, blank, blank, blank, blank, blank, blank],
-      [blank, blank, blank, blank, blank, blank, blank, blank],
-      [blank, blank, a, a, a, a, blank, blank],
-      [blank, a, a, a, a, a, a, blank],
+      [none, v, v, v, v, v, v, none],
+      [none, none, v, v, v, v, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, a, a, a, a, none, none],
+      [none, a, a, a, a, a, a, none],
       [a, a, a, a, a, a, a, a]]).
 
+middle([[none, none, none, none, none, v, none, none],
+      [v, v, v, v, none, none, none, none],
+      [none, none, none, none, v, none, none, none],
+      [none, a, none, a, none, v, none, none],
+      [none, none, none, a, a, none, none, none],
+      [none, none, none, a, a, none, v, none],
+      [none, none, none, none, a, a, v, a],
+      [none, none, none, none, none, none, none, none]]).
 
-board_display(X,[L2|L2s]) :- write(X), Y is X+1, write('- '), display_line(L2), nl, display_line__(L2), nl, board_display(Y,L2s).
+end([[qa, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none],
+      [none, none, none, none, none, none, none, none]]).
+
+
+% =========== Display ===========
+board_display(X,[L2|L2s]) :- write(X),
+  Y is X+1,
+  write('| '),
+  display_line(L2), nl,
+  write(' '),
+  display_underline(L2), nl,
+  board_display(Y,L2s).
 board_display(_,[]) :- nl.
 
-display_line([E|Es]) :- translate(E,V), write(V), write(' | '), display_line(Es).
-display_line([]):- !.
+display_line([E|Es]) :- translate(E,V),
+  write(V),
+  write('  | '),
+  display_line(Es).
+display_line([]).
 
-display_line__([_|Es]) :- write(' - -'), display_line__(Es).
-display_line__([]):- !.
+display_underline([_|Es]) :- write('------'), display_underline(Es).
+display_underline([]) :- write(' ').
 
-display_first_line(Inicio, Fim):- write('   '), Y is Inicio + 0, Inicio =< Fim, char_code(Imprime, Y), write(Imprime), X is Y +1, display_first_line(X, Fim).
-display_first_line(_, _).
+display_first_line(Inicio, Fim, _):- write('    '),
+  Y is Inicio + 0,
+  Inicio =< Fim,
+  char_code(Imprime, Y),
+  write(Imprime),
+  write(' '),
+  X is Y +1,
+  display_first_line(X, Fim).
+display_first_line(_, _, B1) :- write(' '), display_underline(B1), nl.
 
-translate(blank,' ').
-translate(v,'V').
-translate(a,'A').
+translate(none,'  ').
+translate(v,' V').
+translate(a,' A').
+translate(qv,'qV').
+translate(qa,'qA').
