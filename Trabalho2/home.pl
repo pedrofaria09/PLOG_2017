c5f698bd27be03cae365414ce9595c05e6019efb
Pedro Faria <pfaf09@gmail.com>
2017-12-15 15:05:34 +0000

    Trying to do it.

--- Trabalho2/home.pl
+++ Trabalho2/home.pl
@@ -1,7 +1,13 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 
-magic(Vars):-
+magic:-
+L1=9,
+L2=7,
+L3=2,
+L4=10,
+L5=3,
+L6=1,
 Linha1=[A1,A2,A3,A4,A5,A6],
 Linha2=[B1,B2,B3,B4,B5,B6],
 Linha3=[C1,C2,C3,C4,C5,C6],
@@ -15,18 +21,18 @@ Coluna4=[A4,B4,C4,D4,E4,F4],
 Coluna5=[A5,B5,C5,D5,E5,F5],
 Coluna6=[A6,B6,C6,D6,E6,F6],
 
-domain(Linha1,0,6),
-domain(Linha2,0,6),
-domain(Linha3,0,6),
-domain(Linha4,0,6),
-domain(Linha5,0,6),
-domain(Linha6,0,6),
-domain(Coluna1,0,6),
-domain(Coluna2,0,6),
-domain(Coluna3,0,6),
-domain(Coluna4,0,6),
-domain(Coluna5,0,6),
-domain(Coluna6,0,6),
+domain(Linha1,-1,4),
+domain(Linha2,-1,4),
+domain(Linha3,-1,4),
+domain(Linha4,-1,4),
+domain(Linha5,-1,4),
+domain(Linha6,-1,4),
+domain(Coluna1,-1,4),
+domain(Coluna2,-1,4),
+domain(Coluna3,-1,4),
+domain(Coluna4,-1,4),
+domain(Coluna5,-1,4),
+domain(Coluna6,-1,4),
 
 all_distinct(Linha1),
 all_distinct(Linha2),
@@ -41,11 +47,39 @@ all_distinct(Coluna4),
 all_distinct(Coluna5),
 all_distinct(Coluna6),
 
+verifylist(Linha1,SubLinha1),
+sum(SubLinha1,#=,L1),
+verifylist(Linha2,SubLinha2),
+sum(SubLinha2,#=,L2),
+
 append([Linha1,Linha2,Linha3,Linha4,Linha5,Linha6],Vars),
 
-%exactly_two_zeros(Vars),
-
 labeling([],Vars), printValues(Vars).
+
+
+verifylist([],[]).
+verifylist([Primeiro|Resto],SubLista):-
+  ((Primeiro #= -1) #\/ (Primeiro #= 0)),
+  coletar(Resto,SubLista).
+verifylist([_|Resto],[],SubLista):-
+  verifylist(Resto,SubLista).
+
+
+coletar([],[]).
+%coletar([Primeiro|_],_):-((Primeiro #= -1) #\/ (Primeiro #= 0)).
+coletar([Primeiro|Resto],Lista):-
+  coletar(Resto,Temp),
+  ((Primeiro #\= -1) #\/ (Primeiro #\= 0)),Lista=[Primeiro|Temp].
+
+/*coletar(Resto,SubLista):-
+  coletar_aux(Resto,[],SubLista).
+
+coletar_aux([],L,L).
+coletar_aux([H|_],L,L):-((H #= -1) #\/ (H #= 0)).
+coletar_aux([H|T],Temp,SubLista):-
+  ((H #\= -1) #\/ (H #\= 0)),
+  append(Temp,[H],SubLista),
+  coletar_aux(T,SubLista,SubLista).*/
 
 test(Vars):-
   length(Vars,5), exactly_two_zeros(Vars), labeling([],Vars).
